782d2ee85310153f5a5a47175abef7e78c4240a3
roses1089 <52487873+roses1089@users.noreply.github.com>
2019-08-22 14:54:26 +0100

    removal of SPN

--- kubernetes/aksclusters.tf
+++ kubernetes/aksclusters.tf
@@ -1,15 +1,10 @@
-/*
-    There should probably only be a single cluster - if this changes it would be obvious
-*/
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.project}-aks-${var.environment}"
   resource_group_name = "${azurerm_resource_group.aks.name}"
   location            = "${var.region}"
   dns_prefix          = "${var.project}aks${var.environment}"
 
-  /*
-    You can only have 100 agents (VMs) per pool, its incredibly unlikely you will need more than one pool.
-  */
+
   agent_pool_profile {
     name            = "gpit1"
     count           = 1
