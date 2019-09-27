580d6c4a934792cda623b8a5cb2115c3b288654e
roses1089 <52487873+roses1089@users.noreply.github.com>
2019-09-27 14:14:56 +0100

    updated demo

--- dev/resource_sql.tf
+++ dev/resource_sql.tf
@@ -50,3 +50,19 @@ resource "azurerm_sql_database" "bc-sql" {
 
   }
 }
+
+resource "azurerm_sql_database" "bc-sql1" {
+  name                              = "${var.project}-db01-${var.environment}"
+  resource_group_name               = "${azurerm_resource_group.bc-sql.name}"
+  location                          = "${var.region}"
+  server_name                       = "${azurerm_sql_server.bc-sql.name}"
+  collation                         = "SQL_Latin1_General_CP1_CI_AS"
+  edition                           = "Standard"
+  requested_service_objective_name  = "S1"
+  threat_detection_policy {
+    state                           = "enabled"
+    storage_endpoint                = "${azurerm_storage_account.sql.primary_blob_endpoint}"
+    storage_account_access_key      = "${azurerm_storage_account.sql.primary_access_key}"
+
+  }
+}
