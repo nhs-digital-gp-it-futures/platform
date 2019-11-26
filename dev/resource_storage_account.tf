440bc0098cffba6c45f159ad7601640b3d30f322
Sean Rose <sean.rose@bjss.com>
2019-11-26 11:19:25 +0000

    Added azure lock to storage_account.tf

--- dev/resource_storage_account.tf
+++ dev/resource_storage_account.tf
@@ -6,6 +6,13 @@ resource "azurerm_resource_group" "storage" {
   }
 }
 
+resource "azurerm_management_lock" "aks" {
+  name = "CanNotDelete"
+  scope = "${azurerm_resource_group.storage.id}"
+  lock_level = "CanNotDelete"
+  notes = "Azure Lock for Dev Storage Account"
+}
+
 resource "azurerm_storage_account" "data" {
   name                      = "${var.project}sa${var.environment}"
   location                  = "${var.region}"
