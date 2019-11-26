003f7d969915c4ceb418f79a53414f075a01cd1b
Sean Rose <sean.rose@bjss.com>
2019-11-26 11:54:56 +0000

    Added yml file for namespace, also added 2nd publicIp address

--- dev/resource_virtual_network.tf
+++ dev/resource_virtual_network.tf
@@ -109,9 +109,9 @@ resource "azurerm_public_ip" "Pip" {
 }
 
 resource "azurerm_public_ip" "Pip1" {
-  name                                  = "${var.project}-pip-${var.environment}1"
+  name                                  = "${var.project}-pip1-${var.environment}1"
   location                              = "${var.region}"
-  resource_group_name                   = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name                   = "gpitfuture-akspool-dev"
   allocation_method                     = "Static"
   sku                                   = "Standard"
   tags                                  = {
