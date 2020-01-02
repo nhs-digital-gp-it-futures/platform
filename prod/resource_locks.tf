b58b7d64c76d00d149773f4c494ec7865e998da5
Sean Rose <sean.rose@bjss.com>
2020-01-02 16:10:55 +0000

    Delete resource_locks.tf

--- prod/resource_locks.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_management_lock" "storage" {
-  name                              = "lock_storage_account"
-  scope                             = "/subscriptions/7b12a8a2-f06f-456f-b6f9-aa2d92e0b2ec/resourceGroups/gpitfutures-prod-rg-sa"
-  lock_level                        = "CanNotDelete"
-  notes                             = "This resource group has been set to DoNotDelete to prevent terraform tfstate files and static content from being deleted"
-}
