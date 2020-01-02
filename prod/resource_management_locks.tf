b763420e0991334b5de84ca724b8460202969ad6
Sean Rose <sean.rose@bjss.com>
2020-01-02 17:00:48 +0000

    Update resource_management_locks.tf

--- prod/resource_management_locks.tf
+++ prod/resource_management_locks.tf
@@ -1,62 +1,76 @@
+resource "azurerm_management_lock" "acr" {
+    name            = "az_mgm_lock_aks"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-acr"
+    lock_level      = "CanNotDelete"
+    notes           = "To prevent Production Resource Group from being deleted"
+}
+
 resource "azurerm_management_lock" "aks" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.aks.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-aks"
+    lock_level      = "CanNotDelete"
+    notes           = "To prevent Production Resource Group from being deleted"
+}
+
+resource "azurerm_management_lock" "akspool" {
+    name            = "az_mgm_lock_aks_pool"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-akspool"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "ehubuks" {
     name            = "az_mgm_lock_ehub_uks"
-    scope           = "${azurerm_resource_group.ehubuks.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-ehub-uks"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "ehubukw" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.ehubukw.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-ehub-ukw"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "log_analytics" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.log_analytics.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-la"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "bc-sql-pri" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.bc-sql-pri.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-sql-pri"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "bc-sql-sec" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.bc-sql-sec.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-sql-sec"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "storage" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.storage.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-sa"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "virtual_machine" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.virtual_machine.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-vm"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
 
 resource "azurerm_management_lock" "vnet" {
     name            = "az_mgm_lock_aks"
-    scope           = "${azurerm_resource_group.vnet.id}"
+    scope           = "/subscriptions/d1be8dbc-1a9f-4b7b-ba51-037116110e00/resourceGroups/gpitfutures-prod-rg-vnet"
     lock_level      = "CanNotDelete"
     notes           = "To prevent Production Resource Group from being deleted"
 }
