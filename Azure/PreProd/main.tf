7262bebc6149e22e3785e92a1eee6e04c864fbf8
Sean Rose <sean.rose@bjss.com>
2020-02-10 13:48:49 +0000

    Updates to the directory for structure for the platform

---
+++ Azure/PreProd/main.tf
@@ -0,0 +1,19 @@
+#The Terraform tfstate file is currently being stored in an Azure storage container.
+#Storage account name: gpitfuturesatf within the GP IT Futures Buying Catalogue subscription
+
+terraform {
+  required_version        = ">= 0.12"
+}
+
+provider "azurerm" {
+  version                 = "=1.42.0"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "gpitfutures-rg-sa"
+    storage_account_name  = "gpitfuturessatf"
+    container_name        = "tfstate"
+    key                   = "pprod.terraform.tfstate"
+  }
+}
