1c7860ad17fba0c0d05c5dc7c43b86c50411e184
PhysicsJon <j.r.alsop.18@gmail.com>
2020-03-13 14:50:08 +0000

    Updating for az deployment (hard coded)

---
+++ local-helm/launch-or-update-environment-az.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+helm upgrade bc buyingcatalogue -n buyingcatalogue -i -f az-overrides.yaml --wait $@
+
+kubectl port-forward svc/gpitfutures-bc-bapi-sql 1450:1450 -n buyingcatalogue &
+kubectl port-forward svc/gpitfutures-bc-isapi-sql 1451:1451 -n buyingcatalogue &
+#kubectl port-forward svc/gpitfutures-bc-mp 3002:3002 -n buyingcatalogue &
+kubectl port-forward svc/gpitfutures-bc-pb 3000:3000 -n buyingcatalogue &
+kubectl port-forward svc/gpitfutures-bc-isapi 5102:5102 -n buyingcatalogue &
+wait
