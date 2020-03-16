f9dbe9b55a70a24ec0984c7eef9cc940740c9185
stuart-bass-bjss <59572183+stuart-bass-bjss@users.noreply.github.com>
2020-03-16 10:39:57 +0000

    working local https

--- local-helm/launch-or-update-environment-az.sh
+++ local-helm/launch-or-update-environment-az.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm upgrade bc buyingcatalogue -n buyingcatalogue -i -f az-overrides.yaml --wait $@
+helm upgrade bc buyingcatalogue -n buyingcatalogue -i -f environments/remote-azure.yaml -f az-overrides.yaml --wait $@
 
 kubectl port-forward svc/gpitfutures-bc-bapi-sql 1450:1450 -n buyingcatalogue &
 kubectl port-forward svc/gpitfutures-bc-isapi-sql 1451:1451 -n buyingcatalogue &
