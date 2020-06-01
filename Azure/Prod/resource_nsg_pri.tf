a94479e337a5c8b3faac632c68438e863abb1615
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-06-01 08:56:02 +0100

    Updated NSG rule GovWfh_pri to fix prefix bug

--- Azure/Prod/resource_nsg_pri.tf
+++ Azure/Prod/resource_nsg_pri.tf
@@ -48,7 +48,7 @@ resource "azurerm_network_security_rule" "GovWfh_pri" {
   name                        = "AllowGovWfh"
   resource_group_name         = azurerm_resource_group.vnet.name
   network_security_group_name = azurerm_network_security_group.gateway_pri.name
-  source_address_prefix       = var.gov_wfh_ip_add
+  source_address_prefixes     = var.gov_wfh_ip_add
   destination_address_prefix  = "*"
   source_port_range           = "*"
   destination_port_ranges     = [ "80", "433"  ]
