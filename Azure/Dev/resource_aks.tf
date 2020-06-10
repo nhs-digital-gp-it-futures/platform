0b7136a075350dfd2ef6419784be68528643ab40
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-06-10 21:00:42 +0100

    Added devpool2 and reduced count to 1

--- Azure/Dev/resource_aks.tf
+++ Azure/Dev/resource_aks.tf
@@ -42,7 +42,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     enable_auto_scaling           = "false"
     #max_count                     = 6
     #min_count                     = 4 
-    node_count                    = 7
+    node_count                    = 1
    #enable_node_public_ip         = "true"
   }
 
@@ -88,8 +88,9 @@ resource "azurerm_kubernetes_cluster_node_pool" "akspool2" {
   name                  = "devpool2"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
   vm_size               = "Standard_B4ms"
-  node_count            = 1
+  node_count            = 3
   enable_auto_scaling   = "false"
+  vnet_subnet_id        = azurerm_subnet.aks.id
   #os_disk_size_gb      = 30
 
   tags = {
